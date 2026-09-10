482629919d0d005c437add9b01cceccb28925d79
AlanSzylko <alanszylkowski@gmail.com>
2026-09-10 12:53:02 -0300

    fix Wanda's name

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -7,7 +7,7 @@ export class SuperheroesList {
     { name: "Thor", side: "hero" },
     { name: "Thanos", side: "villain" },
     { name: "Loki", side: "villain" },
-    { name: "Wanda", side: "villain" },
+    { name: "Wanda Maximoff", side: "villain" },
     { name: "Ultron", side: "villain" },
     { name: "Red Skull", side: "villain" },
   ];
@@ -19,6 +19,7 @@ export class SuperheroesList {
   getVillains(): Superhero[] {
     return this.characters.filter((c) => c.side === "villain");
   }
+
 
   getAll(): Superhero[] {
     return this.characters;
