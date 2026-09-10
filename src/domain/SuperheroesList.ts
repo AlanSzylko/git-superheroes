470b037faba151e40ad1585afd6256389497c6e8
AlanSzylko <alanszylkowski@gmail.com>
2026-09-10 13:01:24 -0300

    Resolve superhero list conflict preserving both versions

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -8,6 +8,9 @@ export class SuperheroesList {
     { name: "Thanos", side: "villain" },
     { name: "Loki", side: "villain" },
     { name: "Wanda Maximoff", side: "villain" },
+    { name: "Scarlet Witch", side: "hero" },
+    { name: "Black widow", side: "hero" },
+    { name: "Hawkeye", side: "hero" },
     { name: "Ultron", side: "villain" },
     { name: "Red Skull", side: "villain" },
   ];
